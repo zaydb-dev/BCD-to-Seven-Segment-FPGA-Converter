800ff6d01f4cf0a608ded3cac39bd657ca7ee962
Zayd B <zaydboutaleb@gmail.com>
2026-01-19 23:10:32 +0000

    Refactor BCD to 7-segment display logic

--- bcd_to_7seg.v
+++ bcd_to_7seg.v
@@ -2,18 +2,19 @@
 // BCD to 7-Segment Display Converter
 
 module lab3_part1(
-    input w, x, y, z,           // 4-bit BCD input 
+    input w, x, y, z, // 4-bit BCD input 
+    output [3:0] select , 
     output a, b, c, d, e, f, g  // 7-segment outputs 
 );
 
     // Data-flow modeling using simplified Boolean expressions derived from K-maps
-    
-    assign a = (x & ~y & ~z) | (~w & ~x & y & z);           // Segment a 
+    assign select = 4'b1110 ;
+    assign a = (x & ~y & ~z) | (~w & ~x & ~y & z);           // Segment a 
     assign b = (x & ~y & z) | (x & y & ~z);                 // Segment b 
     assign c = (~x & y & ~z);                               // Segment c 
-    assign d = (~x & y & z) | (x & ~y & ~z) | (x & y & z);  // Segment d
-    assign e = (x & ~y) | z;                                // Segment e 
+    assign d = (~x & ~y & z) | (x & ~y & ~z) | (x & y & z);  // Segment d
+    assign e = (x & ~y) | (z);                                // Segment e 
     assign f = (~w & ~x & z) | (y & z) | (~x & y);          // Segment f
-    assign g = (w & ~x & ~y) | (x & y & z);                 // Segment g 
+    assign g = (~w & ~x & ~y) | (x & y & z);                 // Segment g 
 
 endmodule
